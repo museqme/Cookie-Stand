f91538967f72270ae89b6d3223c2352ac32a0245
Cameron <cameron.32090@gmail.com>
2017-06-12 14:02:19 -0700

    Project Start

---
+++ app.js
@@ -0,0 +1,18 @@
+var hours = ['6am', '7am'];
+var pikePlaceMkt = {
+  locationName: 'Pike Place Market',
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 65,
+  avgCookiesPerCustomer: 6.3,
+  calcCustomersThisHour: function() {
+
+  },
+  customersEachHour: [],
+  calcCookiesThisHour: function() {
+
+  },
+  cookiesEachHour:[],
+  render: function() {
+    
+  },
+}
